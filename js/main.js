9ccc9ea88bb3d3a6390d0f0a3e51e64a20b77de1
Pavel Ševčík <pajaseviwow@gmail.com>
2014-09-24 19:47:38 +0200

    basic scripts

--- js/main.js
+++ js/main.js
@@ -1,1 +1,45 @@
 var EB = EB || {};
+
+
+
+
+
+
+////////////////////////
+
+$(document).ready(function() {
+
+  var owl = $('.content');
+
+  owl.owlCarousel({
+    navigation : true,
+    slideSpeed : 1000,
+    paginationSpeed : 400,
+    singleItem:true
+  });
+
+  $(window).on('load', function() {
+    if ( window.location.hash != "") {
+      var hash = window.location.hash;
+      var item = $(hash);
+      var index = item.index('section');
+
+      owl.trigger('owl.jumpTo', index);
+    }
+  });
+
+  $(window).on('hashchange', function(e) {
+    e.preventDefault();
+    e.stopPropagation();
+  });
+
+  $('#main-menu').on('click', 'a', function() {
+    var hash = $(this).attr('href');
+    var item = $(hash);
+    var index = item.index('section');
+
+    window.location.hash = hash;
+    owl.trigger('owl.goTo', index);
+  });
+
+});
