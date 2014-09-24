db8db4d467922d2818def0099efabe7a91edcc92
Pavel Ševčík <pajaseviwow@gmail.com>
2014-09-24 21:42:45 +0200

    revoked js function, added working carousel

--- js/main.js
+++ js/main.js
@@ -14,32 +14,18 @@ $(document).ready(function() {
   owl.owlCarousel({
     navigation : true,
     slideSpeed : 1000,
-    paginationSpeed : 400,
-    singleItem:true
+    paginationSpeed : 1000,
+    pagination: false,
+    singleItem: true,
+    rewindNav: false,
+    navigationText: ['předchozí', 'další'],
+    addClassActive: true
   });
 
-  $(window).on('load', function() {
-    if ( window.location.hash != "") {
-      var hash = window.location.hash;
-      var item = $(hash);
-      var index = item.index('section');
-
-      owl.trigger('owl.jumpTo', index);
-    }
-  });
-
-  $(window).on('hashchange', function(e) {
-    e.preventDefault();
-    e.stopPropagation();
-  });
-
-  $('#main-menu').on('click', 'a', function() {
-    var hash = $(this).attr('href');
-    var item = $(hash);
-    var index = item.index('section');
-
-    window.location.hash = hash;
-    owl.trigger('owl.goTo', index);
+  $('#main-menu').on('click', 'li', function() {
+    var item = $(this).data('section');
+    $(this).addClass('active');
+    owl.trigger('owl.goTo', item);
   });
 
 });
